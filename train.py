a990b89caba5cfce251983a55e7172423ea6c188
breadbread1984 <breadbread1984@163.com>
2024-03-07 16:45:20 +0800

    update

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from models import Predictor
 FLAGS = flags.FLAGS
 
 def add_options():
-  flags.DEFINE_string('lr', default = 1e-3, help = 'learning rate')
+  flags.DEFINE_float('lr', default = 1e-3, help = 'learning rate')
   flags.DEFINE_string('dataset', default = 'dataset.tfrecord', help = 'path to tfrecord')
   flags.DEFINE_string('ckpt', default = 'ckpt', help = 'paht to checkpoint')
   flags.DEFINE_integer('epoch', default = 5, help = 'epoch')
