2a2ab7315476e1b02bee34eeae891c2a104d0633
breadbread1984 <breadbread1984@163.com>
2024-03-07 15:46:06 +0800

    update

--- models.py
+++ models.py
@@ -4,9 +4,10 @@ import tensorflow as tf
 
 def Predictor(rnn_layer_num = 2, channel = 32):
   inputs = tf.keras.Input((None, 2)) # inputs.shape = (batch, seq_len, 2)
+  states = [tf.keras.Input((channel,)) for i in range(rnn_layer_num)] # state.shape = (batch, channel)
   rnn = tf.keras.layers.RNN([tf.keras.layers.GRUCell(channel) for i in range(rnn_layer_num)], return_sequence = True, return_state = True)
-  outputs = rnn(inputs)
-  results, state = outputs[0], outputs[1:] # results.shape = (batch, seq_len, channel)
+  outputs = rnn(inputs, initial_state = states)
+  results, latest_states = outputs[0], outputs[1:] # results.shape = (batch, seq_len, channel)
   results = tf.keras.layers.Dense(2)(results) # results.shape = (batch, seq_len, 2)
-  return tf.keras.Model(inputs = inputs, outputs = results)
+  return tf.keras.Model(inputs = inputs, outputs = [results, *latest_states])
 
