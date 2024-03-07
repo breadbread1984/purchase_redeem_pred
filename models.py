6e1968776f1889b47d639498219b757a89476b44
breadbread1984 <breadbread1984@163.com>
2024-03-07 17:18:14 +0800

    update

--- models.py
+++ models.py
@@ -8,6 +8,10 @@ def Predictor(rnn_layer_num = 2, channel = 32):
   rnn = tf.keras.layers.RNN([tf.keras.layers.GRUCell(channel) for i in range(rnn_layer_num)], return_sequences = True, return_state = True)
   outputs = rnn(inputs, initial_state = states)
   results, latest_states = outputs[0], outputs[1:] # results.shape = (batch, seq_len, channel)
+  results = tf.keras.layers.LayerNormalization()(results)
+  results = tf.keras.layers.Dense(16, activation = tf.keras.activations.gelu)(results)
+  results = tf.keras.layers.Dropout(0.1)(results)
+  results = tf.keras.layers.LayerNormalization()(results)
   results = tf.keras.layers.Dense(2)(results) # results.shape = (batch, seq_len, 2)
   return tf.keras.Model(inputs = [inputs, *states], outputs = [results, *latest_states])
 
