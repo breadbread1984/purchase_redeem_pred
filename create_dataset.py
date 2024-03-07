6dd1c6d796b62ec063fc0fa2b14ed85b76af9bc3
breadbread1984 <breadbread1984@163.com>
2024-03-07 15:23:06 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -4,12 +4,13 @@ from absl import flags, app
 from os.path import exists, join
 from csv import DictReader
 import pickle
+import numpy as np
 
 FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('input_csv', default = None, help = 'path to user_balance_table.csv')
-  flags.DEFINE_string('output_pkl', default = 'dataset.pkl', help = 'path to output npy')
+  flags.DEFINE_string('output', default = 'dataset.tfrecord', help = 'path to output npy')
 
 def main(unused_argv):
   samples = dict()
@@ -23,8 +24,16 @@ def main(unused_argv):
   for key, value in samples.items():
     data.append((key, value[0], value[1]))
   data = list(sorted(data, key = lambda x: x[0]))
-  with open(FLAGS.output_pkl, 'wb') as f:
-    f.write(pickle.dumps(data))
+  sequence = np.stack([[d[1] for d in data],
+                       [d[2] for d in data]], axis = -1) # sequence.shape = (seq_len, 2)
+  writer = tf.io.TFRecordWriter(FLAGS.output)
+  sample = tf.train.Example(features = tf.train.Features(
+    feature = {
+      'x': tf.train.Feature(bytes_list = tf.train.BytesList(value = [tf.io.serialize_tensor(sequence).numpy()]))
+    }
+  ))
+  writer.write(sample.SerializeToString())
+  writer.close()
 
 if __name__ == "__main__":
   add_options()
